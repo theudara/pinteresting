39b29667dfd2da82fb361774ab1b592b70734db8
theudara <udara.africa@gmail.com>
2014-08-24 10:07:37 -0400

    Update show view, add glyphicons, and changed root path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,4 @@ class PagesController < ApplicationController
 
   def about
   end
-
-  def info
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about" #creates about_path
-  get "info" => "pages#info" #creates info_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
